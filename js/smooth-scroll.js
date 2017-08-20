a8f330fdd513cbee48469bbd69f2ff34d38b8585
fisnik <fisnikcanaj2@gmail.com>
2017-08-20 09:51:34 +0200

    Responsive-Bootstrap-master

---
+++ js/smooth-scroll.js
@@ -0,0 +1,31 @@
+
+$(document).ready(function(){
+
+  $(function () {
+    $(document).scroll(function () {
+      var $nav = $(".navbar-fixed-top");
+      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+  });
+
+
+  $("#myNavbar ul li a[href^='#']").on('click', function(e) {
+
+     // prevent default anchor click behavior
+     e.preventDefault();
+
+     // store hash
+     var hash = this.hash;
+
+     // animate
+     $('html, body').animate({
+         scrollTop: $(hash).offset().top
+       }, 1200, function(){
+
+         // when done, add hash to url
+         // (default click behaviour)
+         window.location.hash = hash;
+       });
+
+  });
+});
